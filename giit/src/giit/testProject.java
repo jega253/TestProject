31424b32d67b4b2b8ed794a903ee176a33bdb21a
jega <jega@jega.domain.name>
2023-03-21 11:14:01 +0530

    hello

--- giit/src/giit/testProject.java
+++ giit/src/giit/testProject.java
@@ -4,6 +4,6 @@ public class testProject {
 	public static void main(String[] args) {
 		System.out.println("hi");
 		System.out.println("bye");
-		System.out.println("daiii");
+		System.out.println("hello");
 	}
 }
